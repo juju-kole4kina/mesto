3c59846db5b1efa3f7688414250bf7133cbad2a9
Juliya Koltsova <juliyaasking009@gmail.com>
2023-02-15 17:48:38 +0200

    style: подправлены стили блока gallery

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,33 @@
+'use strict';
+
 let editBtn = document.querySelector('.profile__edit-btn');
 let editPopup = document.querySelector('.popup');
 let exitBtn = document.querySelector('.popup__exit-btn');
 let userName = document.querySelector('.profile__user-name');
-let userDescription = document.querySelector('.profile__user-description');
+let userJob = document.querySelector('.profile__user-description');
 let safeBtn = document.querySelector('.popup__safe-btn');
-let userNameInput = document.querySelector('#user-name');
-let userDescriptionInput = document.querySelector('#user-description');
+let formElement = document.querySelector('.popup__form');
+let userNameInput = formElement.querySelector('#user-name');
+let userJobInput = formElement.querySelector('#user-description');
 
-function editOpen(evt) {
-  evt.preventDefault();
+function editOpen() {
   editPopup.classList.add('popup_opened');
   userNameInput.value = userName.textContent;
-  userDescriptionInput.value = userDescription.textContent;
+  userJobInput.value = userJob.textContent;
 };
 
-function editClose(evt) {
-  evt.preventDefault();
+function editClose() {
   editPopup.classList.remove('popup_opened');
 };
 
 function editProfile(evt) {
   evt.preventDefault();
   userName.textContent = userNameInput.value;
-  userDescription.textContent = userDescriptionInput.value;
+  userJob.textContent = userJobInput.value;
+
+  editClose();
 }
 
 editBtn.addEventListener('click', editOpen);
 exitBtn.addEventListener('click', editClose);
-safeBtn.addEventListener('click', editProfile);
+formElement.addEventListener('submit', editProfile);
