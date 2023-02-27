af0bca12e53a7bb8c45ed3e8906608cbf1b61600
Juliya Koltsova <juliyaasking009@gmail.com>
2023-02-27 04:03:34 +0200

    script: добавление карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,16 @@ let exitBtn = document.querySelectorAll('.popup__exit-btn');
 let userName = document.querySelector('.profile__user-name');
 let userJob = document.querySelector('.profile__user-description');
 let safeBtn = document.querySelector('.popup__safe-btn');
-let formElement = document.querySelector('.popup__form');
-let userNameInput = formElement.querySelector('#user-name');
+let formElement = document.querySelectorAll('.popup__form');
+let userNameInput = document.querySelector('#user-name');
 
 const editprofilePopup = document.querySelector('.popup_type_edit-profile');
 const addCardProfile = document.querySelector('.popup_type_add-card');
 
-let userJobInput = formElement.querySelector('#user-description');
+const galleryList = document.querySelector('.gallery__list');
+const cardTemplate = document.querySelector('.template-card').content;
+
+let userJobInput = document.querySelector('#user-description');
 const initialCards = [
   {
     name: 'Архыз',
@@ -41,6 +44,24 @@ const initialCards = [
   }
 ];
 
+console.log(cardTemplate);
+
+const tmplImg = cardTemplate.querySelector('.gallery__image');
+console.log(tmplImg);
+
+const tmplTitle = cardTemplate.querySelector('.gallery__title');
+console.log(tmplTitle);
+
+initialCards.forEach( function (element) {
+  const cardsElement = cardTemplate.cloneNode(true);
+
+  cardsElement.querySelector('.gallery__image').setAttribute('src', element.link);
+  cardsElement.querySelector('.gallery__image').setAttribute('alt', element.name);
+  cardsElement.querySelector('.gallery__item-title').textContent = element.name;
+
+  galleryList.append(cardsElement);
+})
+
 const popupOpen = () => {
   // popup.classList.add('popup_opened');
   editBtn.addEventListener('click', function() {
@@ -54,6 +75,8 @@ const popupOpen = () => {
 }
 
 const popupClose = () => {
+  // popupType.classList.remove('popup_opened');
+
   exitBtn[0].addEventListener('click', function() {
     popups[0].classList.remove('popup_opened');
   });
@@ -64,7 +87,6 @@ const popupClose = () => {
     popups[2].classList.remove('popup_opened');
   });
 }
-
 // const popupClose = () => {
 //   popup.classList.remove('popup_opened');
 // }
@@ -77,19 +99,60 @@ const editOpen = () => {
 
 const editProfile = (evt) => {
   evt.preventDefault();
+  let parentSection = evt.srcElement.closest('section.popup');
   userName.textContent = userNameInput.value;
   userJob.textContent = userJobInput.value;
-
-  popups[0].classList.remove('popup_opened');
+  parentSection.classList.remove('popup_opened');
+  // popups[0].classList.remove('popup_opened');
+  // popupClose();
 }
+
+// const addCard = () => {
+// const cardTemplate = document.querySelector('.template-card');
+// const cardElement = cardTemplate.querySelector('.gallery__item').cloneNode(true);
+
+// cardElement.querySelector('gallery__image').setAttribute('src', '');
+// cardElement.querySelector('.gallery__item-title').textContent = '';
+
+
+// }
+
+const addData = (evt) => {
+  evt.preventDefault();
+  let parentSection = evt.srcElement.closest('section.popup');
+  // parentSection
+  console.log(evt);
+  const cardName = document.querySelector('#title-card').value;
+  const cardImg = document.querySelector('#card-image').value;
+console.log(cardName);
+console.log(cardImg);
+initialCards.unshift({name: `${cardName}`, link: `${cardImg}`});
+console.log(initialCards);
+//  popups[1].classList.remove('popup_opened');
+parentSection.classList.remove('popup_opened');
+// popupClose();
+}
+
+
+
+// addData();
+console.log(initialCards);
 
 // editBtn.addEventListener('click', popupOpen);
 // addCardBtn.addEventListener('click', popupOpen);
 
+// editBtn.addEventListener('click', popupOpen);
+// addCardBtn.addEventListener('click', popupOpen);
+// exitBtn.addEventListener('click', popupClose);
+
 popupOpen();
 popupClose();
-// exitBtn.addEventListener('click', popupClose);
+// addData();
+//
 console.log(popups);
 console.log(exitBtn);
+console.log(formElement);
 
-formElement.addEventListener('submit', editProfile);
+formElement[0].addEventListener('submit', editProfile);
+formElement[1].addEventListener('submit', addData);
+
